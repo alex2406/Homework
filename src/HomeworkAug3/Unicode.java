2d4d4f68ded9f12980c456ff3e1f453ae4a503b9
Alex Cara <scara2406@gmail.com>
2019-08-19 13:16:06 +0300

    2

---
+++ src/HomeworkAug3/Unicode.java
@@ -0,0 +1,14 @@
+package doljoc;
+
+import java.util.Scanner;
+
+public class Unicode {
+
+    public static void main(String[] args) {
+        System.out.println("Enter a character");
+        Scanner input = new Scanner(System.in);
+        char ch = input.next().charAt(0);
+        int castAscii = (int) ch;
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+ }
+}
