86eccded246938838308f81a1494450df2700cc2
Alex Cara <scara2406@gmail.com>
2019-08-21 20:37:50 +0300

    Classwork

---
+++ src/HomeworkAug20/ISBN/ISBNdemo.java
@@ -0,0 +1,8 @@
+package HomeworkAug20.ISBN;
+
+public class ISBNdemo {
+    public static void main(String[] args) {
+        ISBNchecker i = new ISBNchecker();
+        System.out.println(i.compteFullLenghtISBN("123456789123"));
+    }
+}
