db0de379190b3236c53a59e3b641b1391796a83b
Alex2406 <alex2406@inbox.ru>
2019-08-27 14:29:43 +0300

    Hw for aug 21

---
+++ src/HomeworkAug21/CountConsOrVowelV2.java
@@ -0,0 +1,27 @@
+package HomeworkAug21;
+
+import java.util.Scanner;
+
+public class CountConsOrVowelV2 {
+    public static void main(String... args) {
+        CountConsOrVowelV2 c = new CountConsOrVowelV2();
+        c.ConsOrVowel();
+    }
+
+
+    public void ConsOrVowel() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String str = s.next();
+        int vCount = 0, cCount = 0;
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u')
+                vCount++;
+            else
+                cCount++;
+        }
+        System.out.println("Number of vowels: " + vCount);
+        System.out.println("Number of consonants: " + cCount);
+    }
+}
---
+++ src/HomeworkAug21/CountUppercaseLetters.java
@@ -0,0 +1,28 @@
+package HomeworkAug21;
+
+import java.util.Scanner;
+
+public class CountUppercaseLetters {
+    public static void main(String[] args) {
+        CountUppercaseLetters c = new CountUppercaseLetters();
+        c.UpperCaseCounter();
+
+    }
+    private void UpperCaseCounter (){
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Give a string ");
+        String input=keyboard.nextLine();
+
+        int upperCase=0;
+        int lowerCase=0;
+
+        for (int k = 0; k < input.length(); k++) {
+            if (Character.isUpperCase(input.charAt(k))) upperCase++;
+            if (Character.isLowerCase(input.charAt(k))) lowerCase++;
+        }
+
+        System.out.printf("There are %d uppercase letters and %d lowercase letters.",upperCase,lowerCase);
+
+    }
+}
